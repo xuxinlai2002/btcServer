e153c645cffab498918b035a7a8595384e972599
xxl <xxl@local.com>
2021-01-30 17:03:32 +0800

    add the param for loop number

--- outputBtc.sh
+++ outputBtc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-for i in {0..499}
+for (( i = 0; i < $2; i++ ))
 do
-    fromNum=$[ $1 + $i*200 ]
-    endNum=$[ $1 + ($i + 1)*200 -1 ]
+    fromNum=$[ $1 + $i*100 ]
+    endNum=$[ $1 + ($i + 1)*100 -1 ]
+    
     strCmd="docker run -v /home/dev/output/:/bitcoin-etl/output bitcoin-etl:latest export_blocks_and_transactions --start-block $fromNum --end-block $endNum --provider-uri http://user:pass@10.0.3.238:8332 --chain bitcoin --transactions-output output/transactions-$fromNum.json"
 
     #echo $strCmd
